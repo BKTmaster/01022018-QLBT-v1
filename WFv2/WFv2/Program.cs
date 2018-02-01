615a59e9e8f5666ad0cc1b0dd881ba71a5b5d2d7
BKTmaster <35751040+BKTmaster@users.noreply.github.com>
2018-02-01 10:17:02 +0700

    asdasd

--- WFv2/WFv2/Program.cs
+++ WFv2/WFv2/Program.cs
@@ -17,6 +17,7 @@ namespace WFv2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            ///sadasd
         }
     }
 }
